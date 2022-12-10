fcb2e48f878960b5160c00873483b6da8cbfe089
Ronan Klyne <ronan.klyne@cazoo.co.uk>
2022-12-10 09:58:54 +0000

    day 10 part 2

--- day10/src/app.test.ts
+++ day10/src/app.test.ts
@@ -4,47 +4,61 @@ type State = [number, number]; // cycles, register
 const newMachine = (): State => [1, 1];
 
 type Instruction = { op: "noop" } | { op: "addx"; x: number };
-const NOOP: Instruction = {op: "noop"}
+const NOOP: Instruction = { op: "noop" };
 const doInstruction = (state: State, instr: Instruction): State[] => {
   const states: State[] = [[state[0] + 1, state[1]]];
-  if (instr.op == 'addx') {
-    states.push([state[0]+2, state[1] + instr.x])
+  if (instr.op == "addx") {
+    states.push([state[0] + 2, state[1] + instr.x]);
   }
   return states;
 };
 
 const run = (instrs: Instruction[], observe: (state: State) => void) => {
   let state = newMachine();
-  instrs.forEach(instr => {
-    doInstruction(state, instr).forEach(newState => {
-      observe(newState)
-      state = newState
-    })
-  })
-}
+  observe(state);
+  instrs.forEach((instr) => {
+    doInstruction(state, instr).forEach((newState) => {
+      observe(newState);
+      state = newState;
+    });
+  });
+};
 
 const parse = (input: string): Instruction[] => {
-  return input.split("\n").map(
-    line => {
-      const t = line.trim()
-      if (t == "noop") return NOOP
-      return {op: "addx", x: parseInt(t.split(" ")[1])}
-    }
-  )
-}
+  return input.split("\n").map((line) => {
+    const t = line.trim();
+    if (t == "noop") return NOOP;
+    return { op: "addx", x: parseInt(t.split(" ")[1]) };
+  });
+};
 
 const part1 = (ins: Instruction[]): number => {
-  let answer = 0
-  const interesting = [
-    20, 60, 100, 140, 180, 220
-  ]
+  let answer = 0;
+  const interesting = [20, 60, 100, 140, 180, 220];
   run(ins, (state) => {
     if (interesting.includes(state[0])) {
-      answer += state[0] * state[1]
+      answer += state[0] * state[1];
     }
-  })
-  return answer
-}
+  });
+  return answer;
+};
+
+const part2 = (ins: Instruction[]): string => {
+  let answer: string[] = [];
+  const interesting = [20, 60, 100, 140, 180, 220];
+  run(ins, (state) => {
+    const x = (state[0] - 1) % 40 + 1
+    if (x >= state[1] && x <= state[1] + 2) {
+      answer.push("#");
+    } else {
+      answer.push(".");
+    }
+    if (x == 40) {
+      answer.push("\n");
+    }
+  });
+  return answer.join("");
+};
 
 describe("day10", () => {
   it("can do a noop", () => {
@@ -58,21 +72,39 @@ describe("day10", () => {
   });
 
   it("can do the tiny program", () => {
-    let lastState: any
-    run([
-      {op: "noop"},
-      {op: "addx", x: 3},
-      {op: "addx", x: -5},
-    ], (state) => {lastState = state})
-    expect(lastState).toStrictEqual([6, -1])
-  })
+    let lastState: any;
+    run(
+      [{ op: "noop" }, { op: "addx", x: 3 }, { op: "addx", x: -5 }],
+      (state) => {
+        lastState = state;
+      }
+    );
+    expect(lastState).toStrictEqual([6, -1]);
+  });
 
-  describe("part1", ()=>{
+  describe("part1", () => {
     it("can do the sample", () => {
-      expect(part1(parse(testData))).toBe(13140)
-    })
+      expect(part1(parse(testData))).toBe(13140);
+    });
     it("can do the problem", () => {
-      expect(part1(parse(input))).toBe(14220)
+      expect(part1(parse(input))).toBe(14220);
+    });
+  });
+
+  describe("part2", () => {
+    it("can do the sample", () => {
+      expect(part2(parse(testData))).toStrictEqual(
+        `##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######.....
+.`
+      );
+    });
+    it("can do the sample", () => {
+      expect(part2(parse(input))).toBe("fun")
     })
-  })
+  });
 });
