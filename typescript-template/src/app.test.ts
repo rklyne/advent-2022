1a35eff165cf14a16ae77da0fcf2070a07775f2b
Ronan Klyne <ronan.klyne@cazoo.co.uk>
2022-12-20 10:32:32 +0000

    template update

--- typescript-template/src/app.test.ts
+++ typescript-template/src/app.test.ts
@@ -1,5 +1,40 @@
-describe('', () => {
-    it('', () => {
+import * as R from "ramda";
+
+import { data, testData } from "./input";
+
+const parse = (text: string) => {
+}
+
+const part1 = (input: any) => {
+  return 0
+}
+
+const part2 = (input: any) => {
+  return 0
+}
+
+describe('day X', () => {
+    it('fails', () => {
         expect(false).toBe(true);
     });
+
+  describe.skip("part 1", () => {
+    it("sample", () => {
+      expect(part1(parse(testData))).toBe(-1)
+    })
+
+    it("answer", () => {
+      expect(part1(parse(data))).toBe(-1)
+    })
+  })
+
+  describe.skip("part 2", () => {
+    it("sample", () => {
+      expect(part2(parse(testData))).toBe(-1)
+    })
+
+    it("answer", () => {
+      expect(part2(parse(data))).toBe(-1)
+    })
+  })
 });
