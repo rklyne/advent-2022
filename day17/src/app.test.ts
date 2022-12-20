fa934d023ac94b47561b7a98420869c168a293a8
Ronan Klyne <ronan.klyne@cazoo.co.uk>
2022-12-20 10:19:07 +0000

    day 17 part 2

--- day17/src/app.test.ts
+++ day17/src/app.test.ts
@@ -60,6 +60,12 @@ const gridPositions = (grid: Grid, offset: Point = [0, 0]): Point[] => {
   return points;
 };
 
+type LoopState = {
+  rows: number;
+  query: string;
+  rockStart: number;
+  rocks?: number;
+};
 type GameState = {
   steps: number;
   highestRock: number;
@@ -73,6 +79,7 @@ type GameState = {
   fallingRockLeft?: number;
   fallingRockHeight?: number;
   log: string;
+  loop?: LoopState;
 };
 
 const makeGame = (moves: Move[]): GameState => {
@@ -96,23 +103,24 @@ const cycleOne = <T>(lst: T[]): T => {
   return next;
 };
 
-const gameStep = (previousState: GameState, logLive = false): GameState => {
+const gameStep = (
+  previousState: GameState,
+  onCycle: (game: GameState) => void
+): GameState => {
   const heightGrowth = 8;
   const game = { ...previousState };
   const log = (msg: string) => {
-    game.log += "\n" + msg;
-    if (logLive) console.log(msg);
+    // game.log += "\n" + msg;
   };
   game.steps += 1;
   const goneTooLow = game.fallingRockHeight > game.highestRock + 10;
   if (goneTooLow) throw "oops algorithm fail";
-  if (game.steps > 1_000_000) {
-    console.log({ game });
-    throw "oops limit";
-  }
 
+  let shifted = 0;
   while (game.grid.length < game.highestRock + heightGrowth) {
     game.grid.unshift(makeRow(game.width));
+    shifted++;
+    if (shifted > 10 + heightGrowth) throw "oops shift";
   }
 
   const currentPos = (): Point => [
@@ -145,6 +153,7 @@ const gameStep = (previousState: GameState, logLive = false): GameState => {
 
   const posLeft: Point = [currentPos()[0], currentPos()[1] - 1];
   const posRight: Point = [currentPos()[0], currentPos()[1] + 1];
+
   const doGasJetPush = () => {
     const nextMove = cycleOne(game.moves);
     if (nextMove == LEFT) {
@@ -167,23 +176,54 @@ const gameStep = (previousState: GameState, logLive = false): GameState => {
     }
   };
 
+  const findCycles = (): void => {
+    const searchGrid = game.grid
+      .map((row) => row.join(""))
+      .filter((row) => row != ".......");
+    const searchStr = searchGrid.join("|");
+    if (game.loop) {
+      if (searchStr.slice(0, game.loop.query.length) == game.loop.query) {
+        // we looped
+        game.loop.rocks = game.rocksDone - game.loop.rockStart;
+        onCycle(game);
+      }
+      return;
+    }
+    const max = game.highestRock;
+    // if (max % 2 != 0) return;
+    const checkLen = game.moves.length * game.rocks.length;
+    const matchLen = 400 * 8;
+    const query = searchStr.slice(0, matchLen);
+    const found = searchStr.indexOf(query, 1);
+    if (found == -1) return;
+    const loopRows = found / 8;
+    game.loop = {
+      rows: loopRows,
+      query,
+      rockStart: game.rocksDone,
+    };
+    // onCycle(game);
+  };
+
   const clearEmpties = (): void => {
+    // I think this is a bad idea.
+    // It keeps memory under control but it prevents cycle detection
     const fullRow = (): number | undefined => {
-      for (const [cells, row] of game.grid
-        .slice(6)
-        .map((cells, rowNum): [Cell[], number] => [cells, rowNum])) {
-        if (R.all((c) => c == ROCK, cells)) return row;
+      for (const r of R.range(0, 10)) {
+        if (!game.grid[r]) return;
+        if (R.all((c) => c == ROCK, game.grid[r])) return r;
       }
     };
-    const row = fullRow()
-    if (!row) return
-    const pre = game.grid.length
-    const removed = game.grid.length - row 
-    game.removedRows += removed
-    game.grid = game.grid.slice(0, row)
+    const row = fullRow();
+    if (!row) return;
+    const pre = game.grid.length;
+    const removed = game.grid.length - row;
+    game.removedRows += removed;
+    game.grid = game.grid.slice(0, row);
+    // console.log(`split ${pre}@${row} => ${game.grid.length} + (${removed})`)
+    // printGame(game, false)
     if (pre != removed + game.grid.length) {
-      console.log(`bad split ${pre}@${row} => ${game.grid.length} + (${removed})`)
-      throw "oops split"
+      throw "oops split";
     }
   };
 
@@ -216,6 +256,7 @@ const gameStep = (previousState: GameState, logLive = false): GameState => {
     // reset
     game.fallingRock = null;
     game.rocksDone += 1;
+    // clearEmpties()
     // compute new max height
     game.highestRock = game.grid.length;
     for (const i in R.range(0, heightGrowth)) {
@@ -223,7 +264,7 @@ const gameStep = (previousState: GameState, logLive = false): GameState => {
         game.highestRock--;
       }
     }
-    // clearEmpties()
+    findCycles();
   }
 
   return game;
@@ -245,10 +286,30 @@ const runGame = (
   printEach = false
 ): GameState => {
   let game = makeGame(moves);
-  while (game.rocksDone < MAX_ROCKS) {
-    game = gameStep(game);
+  let max = MAX_ROCKS;
+  let rowBoost = 0;
+  while (game.rocksDone < max) {
+    game = gameStep(game, (game) => {
+      if (rowBoost) return
+      const remainingRocks = MAX_ROCKS - game.rocksDone
+      const skipCycles = Math.floor(remainingRocks / game.loop.rocks)
+      const skipRocks = (game.loop.rocks * skipCycles)
+      rowBoost = skipCycles * game.loop.rows
+
+      console.log({
+        msg: "loop detected",
+        currentHighestRock: game.highestRock,
+        loop_rocks: game.loop.rocks,
+        loop_rows: game.loop.rows,
+        skipRocks,
+        rowBoost,
+        remainingRocks,
+      });
+      max -= skipRocks
+    });
     if (printEach) printGame(game, false);
   }
+  game.highestRock += rowBoost
   return game;
 };
 
@@ -257,12 +318,17 @@ const part1 = (moves: Move[]) => {
   const game = runGame(moves, MAX_ROCKS);
   // printGame(game, false);
   const total = game.highestRock + game.removedRows;
-  console.log({high: game.highestRock, removed: game.removedRows, total})
-  return total
+  // console.log({ high: game.highestRock, removed: game.removedRows, total });
+  return total;
 };
 
 const part2 = (moves: Move[]) => {
   const MAX_ROCKS = 1_000_000_000_000;
+  // const MAX_ROCKS = 10_000;
+  const game = runGame(moves, MAX_ROCKS);
+  const total = game.highestRock + game.removedRows;
+  // console.log({ high: game.highestRock, removed: game.removedRows, total });
+  return total;
 };
 
 describe("", () => {
@@ -296,6 +362,15 @@ describe("", () => {
     });
     it("answer", () => {
       expect(part1(parse(data))).toBe(3144);
+    });
+  });
+
+  describe("part2", () => {
+    it("sample", () => {
+      expect(part2(parse(testData))).toBe(1514285714288);
+    });
+    it("answer", () => {
+      expect(part2(parse(data))).toBe(1565242165201);
     });
   });
 });
