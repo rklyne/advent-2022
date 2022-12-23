25dd0d2bf058c4acb7fb9e835a35bb52c004e680
Ronan Klyne <ronan.klyne@cazoo.co.uk>
2022-12-23 21:29:39 +0000

    template update

--- typescript-template/src/app.test.ts
+++ typescript-template/src/app.test.ts
@@ -2,39 +2,40 @@ import * as R from "ramda";
 
 import { data, testData } from "./input";
 
-const parse = (text: string) => {
-}
+type Input = any;
 
-const part1 = (input: any) => {
-  return 0
-}
+const parse = (text: string): Input => {};
 
-const part2 = (input: any) => {
-  return 0
-}
+const part1 = (input: Input) => {
+  return 0;
+};
 
-describe('day X', () => {
-    it('fails', () => {
-        expect(false).toBe(true);
-    });
+const part2 = (input: Input) => {
+  return 0;
+};
+
+describe("day X", () => {
+  it("fails", () => {
+    expect(false).toBe(true);
+  });
 
   describe.skip("part 1", () => {
     it("sample", () => {
-      expect(part1(parse(testData))).toBe(-1)
-    })
+      expect(part1(parse(testData))).toBe(-1);
+    });
 
-    it("answer", () => {
-      expect(part1(parse(data))).toBe(-1)
-    })
-  })
+    it.skip("answer", () => {
+      expect(part1(parse(data))).toBe(-1);
+    });
+  });
 
   describe.skip("part 2", () => {
     it("sample", () => {
-      expect(part2(parse(testData))).toBe(-1)
-    })
+      expect(part2(parse(testData))).toBe(-1);
+    });
 
-    it("answer", () => {
-      expect(part2(parse(data))).toBe(-1)
-    })
-  })
+    it.skip("answer", () => {
+      expect(part2(parse(data))).toBe(-1);
+    });
+  });
 });
