79a787ba26657e107468a79615c4f9f2db5852bf
Ronan Klyne <ronan.klyne@cazoo.co.uk>
2022-12-23 00:03:24 +0000

    day 22 part 1

--- day22/src/app.test.ts
+++ day22/src/app.test.ts
@@ -41,8 +41,13 @@ class Board {
   #cols: string[];
 
   constructor(private setup: BoardSetup) {
-    this.#rows = setup.map((line) => line.join(""));
-    this.#cols = R.transpose(setup).map((line) => line.join(""));
+    const tSetup = R.transpose(setup)
+    const rMax = Math.max(...setup.map(line => line.length))
+    const rPad = " ".repeat(rMax)
+    const cMax = Math.max(...tSetup.map(line => line.length))
+    const cPad = " ".repeat(cMax)
+    this.#rows = setup.map((line) => (line.join("") + rPad).slice(0, rMax));
+    this.#cols = tSetup.map((line) => (line.join("") + cPad).slice(0, cMax));
   }
 
   get columns(): number {
@@ -74,8 +79,8 @@ class Cursor {
 
   get pos() {
     return {
-      row: this.#row,
-      col: this.#col,
+      row: this.#row + 1,
+      col: this.#col + 1,
       facing: this.#face,
     };
   }
@@ -94,14 +99,17 @@ class Cursor {
     this.#row = doMove(col, this.#row, n);
   }
 
-  private moveRight(n: number) {}
+  private moveRight(n: number) {
+    const row = this.board.getRow(this.#row);
+    this.#col = doMove(row, this.#col, n);
+  }
 
   turn(turn: Turn): void {
     this.#face += 4;
     if (turn == LEFT) {
-      this.#face += 1;
-    } else if (turn == RIGHT) {
       this.#face -= 1;
+    } else if (turn == RIGHT) {
+      this.#face += 1;
     }
     this.#face %= 4;
   }
@@ -113,6 +121,7 @@ const followPath = ([setup, path]: Input) => {
   for (const [move, turn] of path) {
     cursor.move(move);
     cursor.turn(turn);
+    // console.log(cursor.pos)
   }
   return cursor.pos;
 };
@@ -132,72 +141,47 @@ const countChars = (chr: string, text: string): number => {
   return text.split(chr).length - 1;
 };
 
-const doMove = (line: string, start: number, move: number) => {
+const doMove = (line: string, start: number, move: number): number => {
   if (move < 0) {
     const revPos = (n: number) => line.length - n - 1;
-    const revLine = R.reverse(line)
-    const revStart = revPos(start)
+    const revLine = R.reverse(line);
+    const revStart = revPos(start);
     const revMove = doMove(revLine, revStart, -move);
     const result = revPos(revMove);
     // console.log({revLine, revPos, revStart, move, revMove, result});
-    return result
+    return result;
   }
-  return doMoveIter(line, start, move);
+  const result = doMoveIter(line, start, move);
+  if (line[result] != ".") {
+    console.log({msg: "bad result", line, start, chr: line[result], move, result})
+    throw "oops bad result"
+  }
+  return result
   // return doMoveRecurse(line, start, move);
-};
-
-const doMoveRecurse = (
-  line: string,
-  start: number,
-  move: number,
-  log = false
-) => {
-  if (move == 0) {
-    return start;
-  } else if (start >= line.length) {
-    return doMove(line, start % line.length, move);
-  }
-  const TOO_FAR = "too far";
-  const chr = line[start];
-  try {
-    if (chr == " ") {
-      return doMove(line, start + 1, move);
-    } else if (chr == ".") {
-      return doMove(line, start + 1, move - 1);
-    }
-  } catch (err) {
-    if (err == TOO_FAR) {
-      return start;
-    }
-    throw err;
-  }
-  if (chr == "#") {
-    throw TOO_FAR;
-  }
 };
 
 const doMoveIter = (line: string, start: number, move: number) => {
   let pos = start;
   let prev = pos;
-  let text = ""
+  let text = "";
   const log = (msg: string) => {
-    text += `@${pos}(${prev}): ${line[pos]} ${msg}\n`
-  }
-  log(line)
-  while (move > 0) {
+    text += `@${pos}(${prev}): ${line[pos]} ${msg}\n`;
+  };
+  log(line);
+  while (move > 0 || line[pos] != ".") {
     const chr = line[pos];
     if (chr == " ") {
       // prev = pos;
-      log("skipping space")
+      log("skipping space");
       pos += 1;
     } else if (chr == ".") {
-      log("move 1")
+      log("move 1");
       prev = pos;
       pos += 1;
       move -= 1;
     }
     if (line[pos] == "#") {
-      log("rock!")
+      log("rock!");
       // console.log(text)
       return prev;
     }
@@ -243,12 +227,16 @@ describe("day X", () => {
 
     it("stops at rocks while skipping spaces", () => {
       expect(doMove(" ..#. ", 4, 3)).toBe(2);
-    })
+    });
 
     it("wraps", () => {
       expect(doMove(".....", 4, 3)).toBe(2);
       expect(doMove(" ....", 4, 3)).toBe(3);
       expect(doMove(" ....    ", 4, 3)).toBe(3);
+    });
+
+    it("wraps exactly at the end", () => {
+      expect(doMove(" ....", 2, 3)).toBe(1);
     });
 
     it("wraps past the end with blanks and rock", () => {
@@ -266,8 +254,7 @@ describe("day X", () => {
     });
 
     it("answer", () => {
-      expect(part1(parse(data))).toBe(-1);
-      // too high: 149200
+      expect(part1(parse(data))).toBe(131052);
     });
   });
 
