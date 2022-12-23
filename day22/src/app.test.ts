a781f5c8dac780fb11b9f66ade4b8376e7929b92
Ronan Klyne <ronan.klyne@cazoo.co.uk>
2022-12-23 17:58:50 +0000

    day 22 refactors

--- day22/src/app.test.ts
+++ day22/src/app.test.ts
@@ -7,6 +7,10 @@ const LEFT: Turn = "L";
 const RIGHT: Turn = "R";
 
 type Facing = 0 | 1 | 2 | 3; // Right, Down, Left, Up
+const FaceRight: Facing = 0;
+const FaceDown: Facing = 1;
+const FaceLeft: Facing = 2;
+const FaceUp: Facing = 3;
 
 type Cell = " " | "." | "#";
 const isCell = (s: string): s is Cell => {
@@ -36,60 +40,80 @@ const parse = (text: string): Input => {
   return [board, path];
 };
 
-type Position = [number, number, Facing] // row, col, face
-type Line = [Cell, Position][]
+type Position = [number, number, Facing]; // row, col, face
+type Line = [Cell, Position][];
 
 class Board {
   #rows: string[];
   #cols: string[];
+  public rMax: number;
+  public cMax: number;
 
   constructor(private setup: BoardSetup) {
-    const tSetup = R.transpose(setup)
-    const rMax = Math.max(...setup.map(line => line.length))
-    const rPad = " ".repeat(rMax)
-    const cMax = Math.max(...tSetup.map(line => line.length))
-    const cPad = " ".repeat(cMax)
-    this.#rows = setup.map((line) => (line.join("") + rPad).slice(0, rMax));
-    this.#cols = tSetup.map((line) => (line.join("") + cPad).slice(0, cMax));
+    const tSetup = R.transpose(setup);
+    this.rMax = Math.max(...setup.map((line) => line.length));
+    const rPad = " ".repeat(this.rMax);
+    this.cMax = Math.max(...tSetup.map((line) => line.length));
+    const cPad = " ".repeat(this.cMax);
+    this.#rows = setup.map((line) =>
+      (line.join("") + rPad).slice(0, this.rMax)
+    );
+    this.#cols = tSetup.map((line) =>
+      (line.join("") + cPad).slice(0, this.cMax)
+    );
   }
 
   get columns(): number {
     return this.#cols.length;
   }
 
-  getCol(n: number): string {
-    return this.#cols[n];
+  getCol(row: number): string {
+    return this.#cols[row];
   }
 
   get rows(): number {
     return this.#rows.length;
   }
 
-  getRow(n: number): string {
-    return this.#rows[n];
+  getRow(col: number): string {
+    return this.#rows[col];
+  }
+
+  getCell(r: number, c: number): Cell {
+    if (r >= this.rMax) throw new Error("oops bad row num");
+    if (c >= this.cMax) throw new Error("oops bad col num");
+    const row = this.#rows[c];
+    if (!row) throw new Error(`oops not a row ${r}, ${c}, ${row}, (${this.rMax}x${this.cMax})`);
+    const cell = row[r];
+    if (isCell(cell)) return cell;
+    throw new Error(`oops not a cell ${r}, ${c}, ${cell} (${row.length}/${this.rMax}, ${this.cMax})`);
   }
 }
 
+const posButFacing = (pos: Position, face: Facing): Position => {
+  return [pos[0], pos[1], face];
+};
+
 class Cursor {
-  #row: number;
-  #col: number;
-  #face: Facing;
-  constructor(private board: Board) {
-    this.#row = 0;
-    this.#col = board.getRow(0).indexOf(".");
-    this.#face = 0;
+  protected row: number;
+  protected col: number;
+  protected face: Facing;
+  constructor(protected board: Board) {
+    this.row = 0;
+    this.col = board.getRow(0).indexOf(".");
+    this.face = 0;
   }
 
   get pos() {
     return {
-      row: this.#row + 1,
-      col: this.#col + 1,
-      facing: this.#face,
+      row: this.row + 1,
+      col: this.col + 1,
+      facing: this.face,
     };
   }
 
   move(n: number) {
-    const f = this.#face;
+    const f = this.face;
     if (f == 0 || f == 2) {
       this.moveRight(f == 0 ? n : -n);
     } else {
@@ -97,39 +121,166 @@ class Cursor {
     }
   }
 
-  private setPos(pos: Position) {
-    this.#row = pos[0]
-    this.#col = pos[1]
+  public setPos(pos: Position) {
+    if (this.row >= this.board.cMax) throw "oops pos row";
+    if (this.col >= this.board.rMax) throw "oops pos col";
+    this.row = pos[0];
+    this.col = pos[1];
+    this.face = pos[2];
   }
 
-  private getCol(n: number): Line {
-    return this.board.getCol(n).split("").map((c, idx) => [c as Cell, [idx, n, this.#face]])
+  get currentPosition(): Position {
+    return [this.row, this.col, this.face];
   }
 
-  private getRow(n: number): Line {
-    return this.board.getRow(n).split("").map((c, idx) => [c as Cell, [n, idx, this.#face]])
+  get cMax() {
+    return this.board.cMax;
+  }
+  get rMax() {
+    return this.board.rMax;
   }
 
-  private moveDown(n: number) {
-    const col = this.getCol(this.#col);
-    this.setPos( doLineMove(col, this.#row, n));
+  protected getCol(n: number): Line {
+    // const w = this.walk(this.currentPosition, this.board.rMax);
+    // if (w[0][1][2] != this.face) throw "oops walk col facing wrong";
+    // return w
+    if (n >= this.board.cMax) throw new Error("oops getting bad col");
+    const boardCol = this.board.getCol(n);
+    const oldCol: Line = boardCol.split("").map((c, idx) => {
+      if (!isCell(c)) throw "oops walked too far";
+      if (idx >= this.board.cMax)
+        throw new Error(
+          `oops getting bad col/row (${idx} / ${this.board.cMax}) "${boardCol}"`
+        );
+      return [c, [idx, n, this.face]];
+    });
+    return oldCol;
   }
 
-  private moveRight(n: number) {
-    const row = this.getRow(this.#row);
-    this.setPos( doLineMove(row, this.#col, n));
+  protected getRow(n: number): Line {
+    // const w = this.walk(this.currentPosition, this.board.cMax);
+    // if (w[0][1][2] != this.face) throw "oops walk row facing wrong";
+    // return w
+    if (n >= this.board.cMax) throw new Error("oops getting bad row");
+    const boardRow = this.board.getRow(n);
+    const oldRow: Line = boardRow.split("").map((c, idx) => {
+      if (!isCell(c)) throw "oops walked too far";
+      if (idx >= this.board.rMax)
+        throw new Error(
+          `oops getting bad row/col (${idx} / ${this.board.rMax}(${this.board.cMax})) "${boardRow}" > ${boardRow.length}`
+        );
+      return [c, [n, idx, this.face]];
+    });
+    return oldRow;
+  }
+
+  protected next(pos: Position): Position {
+    switch (pos[2]) {
+      case FaceRight:
+        return [pos[0], (pos[1] + 1) % this.rMax, pos[2]];
+      case FaceDown:
+        return [(pos[0] + 1) % this.cMax, pos[1], pos[2]];
+      case FaceLeft:
+        return [
+          pos[0],
+          (pos[1] + this.rMax - 1) % this.rMax,
+          pos[2],
+        ];
+      case FaceUp:
+        return [
+          (pos[0] + this.cMax - 1) % this.cMax,
+          pos[1],
+          pos[2],
+        ];
+    }
+    throw "oops unknown facing";
+  }
+
+  protected walk(pos: Position, n: number): [Cell, Position][] {
+    if (n <= 0) return [];
+    const next = this.next(pos);
+    if (pos[0] > this.cMax) throw new Error("oops bad pos[0]");
+    if (pos[1] > this.rMax) throw new Error("oops bad pos[0]");
+    const cell = this.board.getCell(pos[0], pos[1]);
+    return [[cell, pos], ...this.walk(next, n - 1)];
+    throw "oops unhandled end of walk";
+  }
+
+  protected moveDown(n: number) {
+    const col = this.getCol(this.col);
+    this.setPos(doLineMove(col, this.row, n));
+  }
+
+  protected moveRight(n: number) {
+    const row = this.getRow(this.row);
+    this.setPos(doLineMove(row, this.col, n));
   }
 
   turn(turn: Turn): void {
-    this.#face += 4;
+    this.face += 4;
     if (turn == LEFT) {
-      this.#face -= 1;
+      this.face -= 1;
     } else if (turn == RIGHT) {
-      this.#face += 1;
+      this.face += 1;
     }
-    this.#face %= 4;
+    this.face %= 4;
   }
 }
+
+class BoxCursor extends Cursor {
+  // #row: number;
+  // #col: number;
+  // #face: Facing;
+  #squareSize;
+
+  constructor(board: Board) {
+    super(board);
+    // this.#row = 0;
+    // this.#col = board.getRow(0).indexOf(".");
+    // this.#face = 0;
+    this.#squareSize = Math.max(board.cMax, board.rMax) / 4;
+    console.log({ squareSize: this.#squareSize });
+    if (this.#squareSize != Math.min(board.cMax, board.rMax) / 3)
+      throw "oops square";
+  }
+
+  protected walk(pos: Position, n: number): [Cell, Position][] {
+    if (n <= 0) return [];
+    const next = this.next(pos);
+    const nextChr = this.board.getCell(next[0], next[1]);
+    if (nextChr == " ") throw "oops walk needs rotate";
+    throw "oops unhandled end of walk";
+  }
+
+  protected getCol(n: number): Line {
+    /* squares:
+     * ..#.
+     * ###.
+     * ..##
+     *
+     */
+    // throw "oops not implemented col"
+    return this.walk(this.currentPosition, 4 * this.#squareSize);
+    return this.board
+      .getCol(n)
+      .split("")
+      .map((c, idx) => [c as Cell, [idx, n, this.face]]);
+  }
+
+  protected getRow(n: number): Line {
+    // throw "oops not implemented row"
+    return this.board
+      .getRow(n)
+      .split("")
+      .map((c, idx) => [c as Cell, [n, idx, this.face]]);
+  }
+}
+
+type SquareMap = boolean[][];
+
+const squareAfter = (squareMap: SquareMap, pos: Position) => {
+  const squarePos = [pos[0] / 50];
+};
 
 const followPath = ([setup, path]: Input) => {
   const board = new Board(setup);
@@ -150,7 +301,16 @@ const part1 = (input: Input) => {
 };
 
 const part2 = (input: Input) => {
-  return 0;
+  const [setup, path] = input;
+  const board = new Board(setup);
+  const cursor = new BoxCursor(board);
+  for (const [move, turn] of path) {
+    cursor.move(move);
+    cursor.turn(turn);
+    // console.log(cursor.pos)
+  }
+  const { row, col, facing } = cursor.pos;
+  return 1000 * row + 4 * col + facing;
 };
 
 const countChars = (chr: string, text: string): number => {
@@ -158,10 +318,10 @@ const countChars = (chr: string, text: string): number => {
 };
 
 const doLineMove = (line: Line, start: number, move: number): Position => {
-  const textLine = line.map(c => c[0]).join("")
-  const newPos = doMove(textLine, start, move)
-  return line[newPos][1]
-}
+  const textLine = line.map((c) => c[0]).join("");
+  const newPos = doMove(textLine, start, move);
+  return line[newPos][1];
+};
 
 const doMove = (line: string, start: number, move: number): number => {
   if (move < 0) {
@@ -175,10 +335,17 @@ const doMove = (line: string, start: number, move: number): number => {
   }
   const result = doMoveIter(line, start, move);
   if (line[result] != ".") {
-    console.log({msg: "bad result", line, start, chr: line[result], move, result})
-    throw "oops bad result"
+    console.log({
+      msg: "bad result",
+      line,
+      start,
+      chr: line[result],
+      move,
+      result,
+    });
+    throw "oops bad result";
   }
-  return result
+  return result;
   // return doMoveRecurse(line, start, move);
 };
 
@@ -213,7 +380,7 @@ const doMoveIter = (line: string, start: number, move: number) => {
   return pos;
 };
 
-describe("day X", () => {
+describe("day 22", () => {
   it("can parse", () => {
     const parsed = parse(testData);
     expect(parsed[0][0]).toStrictEqual("        ...#".split(""));
@@ -222,11 +389,6 @@ describe("day X", () => {
   });
 
   describe("moving", () => {
-    // TODO:
-    // - move backward
-    //   - past blanks
-    //   - and rocks
-    // - put it all together in one test
     it("can move", () => {
       expect(doMove("....", 0, 3)).toBe(3);
       expect(doMove(" ...", 1, 2)).toBe(3);
@@ -270,6 +432,40 @@ describe("day X", () => {
     });
   });
 
+  describe.skip("connecting squares", () => {
+    it("can move around a cube", () => {
+      const board = new Board(parse(testData)[0]);
+      const cursor = new BoxCursor(board);
+      cursor.setPos([11, 6, FaceRight]);
+      cursor.move(1);
+      expect(cursor.pos).toStrictEqual([13, 9, FaceDown]);
+    });
+    describe.skip("4x3", () => {
+      const squareMap = [
+        // ..#.
+        // ###.
+        // ..##
+        [false, false, true, false],
+        [true, true, true, false],
+        [false, false, true, true],
+      ];
+
+      it("right from 0,1 is left of 1,1", () => {
+        expect(squareAfter(squareMap, [0, 1, FaceRight])).toStrictEqual({
+          pos: [1, 1],
+          rotation: 0,
+        });
+      });
+
+      it.skip("up from 0,1 is left of 2,0", () => {
+        expect(squareAfter(squareMap, [0, 1, FaceUp])).toStrictEqual({
+          pos: [1, 1],
+          rotation: 0,
+        });
+      });
+    });
+  });
+
   describe("part 1", () => {
     it("sample", () => {
       expect(part1(parse(testData))).toBe(6032);
@@ -282,7 +478,7 @@ describe("day X", () => {
 
   describe.skip("part 2", () => {
     it("sample", () => {
-      expect(part2(parse(testData))).toBe(-1);
+      expect(part2(parse(testData))).toBe(5031);
     });
 
     it.skip("answer", () => {
